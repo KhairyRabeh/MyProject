cf5d038bc6d1ce30015324e9f6aa05edff24d881
KhairyRabeh <khairy_0123456@yahoo.com>
2019-12-25 04:05:52 +0200

    yoyo

---
+++ GitProject.java
@@ -0,0 +1,16 @@
+package gitproject;
+
+public class GitProject {
+    
+    public static void main(String[] args) {
+        printName1();
+        printName2();
+    }
+    
+    static void printName1(){
+        System.out.println("Khairy");
+    }
+    static void printName2(){
+        System.out.println("aa");
+    }
+}
